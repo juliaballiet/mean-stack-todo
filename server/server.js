090ab2b93f775ad242a49f74cdc55545461ac801
Julia Balliet <julia.m.balliet@gmail.com>
2018-08-13 15:56:27 -0500

    updated mongouri

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/todo'
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/todo'
 const toDoRouter = require('./modules/routes/todo.router');
 const PORT = process.env.PORT || 5000;
 
@@ -12,7 +12,7 @@ mongoose.connect(mongoURI, {useNewUrlParser: true});
 mongoose.connection.on('open', () => {
     console.log('connected to Mongo');
 });
-mongoose.connection.on('error', () => {
+mongoose.connection.on('error', (error) => {
     console.log('error connecting: ', error);
 });
 
